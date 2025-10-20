7be0c0eb714f4b569bab720eec936c24a6890a15
chenhang.wch <chenhang.wch@alibaba-inc.com>
2025-10-20 11:28:07 +0800

    userCacheMap

--- src/main/java/com/wch/member/service/impl/UserServiceImpl.java
+++ src/main/java/com/wch/member/service/impl/UserServiceImpl.java
@@ -2,13 +2,16 @@ package com.wch.member.service.impl;
 
 import com.wch.member.entity.User;
 import com.wch.member.error.CacheMap;
+import com.wch.member.error.CacheMap2;
 import com.wch.member.repository.UserRepository;
 import com.wch.member.service.UserService;
+import org.hibernate.Cache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -17,16 +20,29 @@ import java.util.Optional;
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
-    
+
     @Autowired
     private UserRepository userRepository;
-    
+
     @Override
     @Transactional(readOnly = true)
     public Optional<User> findByUsername(String username) {
-        HashMap<String,User> userCacheMap = new HashMap<>();
-        Optional<User> user = userRepository.findByUsername(username);
-        user.ifPresent(value -> userCacheMap.put(username, value));
-        return user;
+        List<User> byIsHotTrue = userRepository.findByIsHotTrue();
+        CacheMap2<User> userCacheMap = new CacheMap2<>();
+        if(byIsHotTrue != null && !byIsHotTrue.isEmpty()){
+            for(User user : byIsHotTrue){
+                userCacheMap.put(user.getUsername(), user, 1000);
+            }
+        }
+        if(userCacheMap.get(username)!=null){
+            return Optional.ofNullable(userCacheMap.get(username));
+        }
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getHotUser() {
+        return userRepository.findByIsHotTrue();
     }
 }
