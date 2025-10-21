d55af179b2d1dfcd5d5b4bb989b67bd29bf50e6e
chenhang.wch <chenhang.wch@alibaba-inc.com>
2025-10-21 16:14:59 +0800

    send_sms_client

--- src/main/java/com/wch/member/service/impl/UserServiceImpl.java
+++ src/main/java/com/wch/member/service/impl/UserServiceImpl.java
@@ -1,5 +1,10 @@
 package com.wch.member.service.impl;
 
+import com.aliyun.teaopenapi.models.Config;
+import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
+import static com.aliyun.teautil.Common.toJSONString;
 import com.wch.member.entity.User;
 import com.wch.member.error.CacheMap;
 import com.wch.member.repository.UserRepository;
@@ -34,6 +39,36 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional(readOnly = true)
     public List<User> findHotUsers() {
+        sendSmsCode();
         return userRepository.findByIsHotTrue();
+    }
+
+    public void sendSmsCode() {
+        try {
+            Config config = new Config()
+                    // 配置 AccessKey ID，请确保代码运行环境配置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID。
+                    .setAccessKeyId(System.getenv("ALIBABA_CLOUD_ACCESS_KEY_ID"))
+                    // 配置 AccessKey Secret，请确保代码运行环境配置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+                    .setAccessKeySecret(System.getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET"));
+            // 配置 Endpoint。
+            config.endpoint = "dysmsapi.aliyuncs.com";
+            // 初始化请求客户端
+            Client client = new Client(config);
+
+            // 构造API请求对象，请替换请求参数值
+            SendSmsRequest sendSmsRequest = new SendSmsRequest()
+                    .setPhoneNumbers("1390000****")
+                    .setSignName("阿里云")
+                    .setTemplateCode("SMS_15305****")
+                    .setTemplateParam("{\"name\":\"张三\",\"number\":\"1390000****\"}"); // TemplateParam为序列化后的JSON字符串。
+
+            // 获取响应对象
+            SendSmsResponse sendSmsResponse = client.sendSms(sendSmsRequest);
+
+            // 响应包含服务端响应的 body 和 headers
+            System.out.println(toJSONString(sendSmsResponse));
+        }catch (Exception e){
+            System.out.println("send sms fail");
+        }
     }
 }
